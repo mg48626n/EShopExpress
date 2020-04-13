4c33340845f0b9a91f7e6e69131d4604dfb96339
Manisha Gupta <manisha@Manishas-MacBook-Air.local>
2020-04-13 14:48:32 -0700

    Created project and added homepage

---
+++ React/eshopexpress/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world...</h1>
+      <div>
+      <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/eshopexpress/src/homepage.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './homepage.scss';
+
+const HomePage = () => (
+
+<div className = "homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className = "content">
+                <h1 className="title"> HAT</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className = "content">
+                <h1 className="title">Jacket</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className = "content">
+                <h1 className="title">Jacket</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className = "content">
+                <h1 className="title">Jacket</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>  
+        <div className="menu-item">
+            <div className = "content">
+                <h1 className="title">Jacket</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+)
+
+export default HomePage;
